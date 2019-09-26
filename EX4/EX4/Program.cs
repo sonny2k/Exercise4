66ae9a411cbf38bc807c04151e155e35569a1f91
sonny2k <thanhdepzaipro@gmail.com>
2019-09-26 15:29:35 +0700

    4a

--- EX4/EX4/Program.cs
+++ EX4/EX4/Program.cs
@@ -6,11 +6,56 @@ using System.Threading.Tasks;
 
 namespace EX4
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+                Console.WriteLine("input n: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                IsOdd(n);
+                IsEven(n);
+                IsPrime(n);
+                Square(n);
+                Cube(n);
+        }
+        public static bool IsOdd( int n) 
+        {
+                if (n%2 != 0) 
+                    return true;
+                else 
+                    return false;  
+        }
+
+        public static bool IsEven(int n)
+        {
+                 if (n % 2 == 0)
+                    return true;
+                 else
+                    return false;  
+        }
+
+        public static bool IsPrime(int n)
+        {
+                int c = 0;
+                for (int i = 1; i <n; i++)
+                {
+                    if (n % i == 0)
+                        c++;
+                }
+                if (c == 2)
+                    return true;
+                else 
+                    return false;
+        }
+
+        public static int Square(int n)
+        {
+                return n * n;
+        }
+
+        public static int Cube (int n)
+        {
+                return n * n * n;
         }
     }
 }
---
+++ EX4/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EX4;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIsOdd()
+        {
+                bool result3 = Program.IsOdd(3);
+                Assert.AreEqual(true, result3);
+
+                bool result4 = Program.IsOdd(4);
+                Assert.AreEqual(false, result4);
+        }
+
+        [TestMethod]
+        public void TestIsEven()
+        {
+                bool result8 = Program.IsEven(8);
+                Assert.AreEqual(true, result8);
+
+                bool result15 = Program.IsEven(15);
+                Assert.AreEqual(false, result15);
+        }
+
+        [TestMethod]
+        public void TestIsPrime()
+        {
+                bool result1 = Program.IsPrime(1);
+                Assert.AreEqual(false, result1);
+                
+                 bool result0 = Program.IsPrime(0);
+                 Assert.AreEqual(false, result0);
+
+                 bool result2 = Program.IsPrime(2);
+                 Assert.AreEqual(false, result2);
+
+                 bool result3 = Program.IsPrime(3);
+                 Assert.AreEqual(false, result3);
+
+                 bool result4 = Program.IsPrime(4);
+                 Assert.AreEqual(true, result4);
+
+                 bool result5 = Program.IsPrime(5);
+                 Assert.AreEqual(false, result5);
+
+                 bool resultNegative = Program.IsPrime(-10);
+                 Assert.AreEqual(false, resultNegative);
+        }
+
+        [TestMethod]
+        public void TestSquare()
+        {
+                int resultNega = Program.Square(2);
+                Assert.AreEqual(4, resultNega);
+        }
+
+        [TestMethod]
+        public void TestCube()
+        {
+                int resultPo = Program.Cube(-2);
+                Assert.AreEqual(-8, resultPo);
+        }
+    }
+}
